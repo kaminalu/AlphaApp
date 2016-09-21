d3661eb6b40de5fa7648ff7f6e1b921e99fd84da
Amy Kamin <akamin@hawk.iit.edu>
2016-09-21 16:05:50 -0500

    Creates Scaffold for Classifications
    Ran the following:
    
    	$ rails g scaffold Classification category:string

---
+++ db/migrate/20160921210527_create_classifications.rb
@@ -0,0 +1,9 @@
+class CreateClassifications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :classifications do |t|
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
