544298400443db6c7bf65a0ca78123f875d8e88e
Amy Kamin <akamin@hawk.iit.edu>
2016-09-26 17:02:06 -0500

    revert back to belongs_to

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
   validates_length_of :article, :within => 1..40
   validates_uniqueness_of :article, :message => "already exists"
   validates :description, presence: true
-  has_and_belongs_to_many :categories
+  belongs_to :category
 end
