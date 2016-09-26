ecd5afef11edec22af91c78c65b2d21586975c93
Amy Kamin <akamin@hawk.iit.edu>
2016-09-26 16:22:20 -0500

    modfiying associations

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,6 +1,5 @@
 class Classification < ApplicationRecord
   validates :category, presence: true
   validates_length_of :category, :within => 1..20
-  validates_uniqueness_of :category, :message => "already exists"
   has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
   validates_length_of :article, :within => 1..40
   validates_uniqueness_of :article, :message => "already exists"
   validates :description, presence: true
-  belongs_to :category
+  has_and_belongs_to_many :categories
 end
